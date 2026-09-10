c1506a58a5aceefa66c55394ea08bf77c8e3b877
Raivis Saldabols <saldabols@pythian.com>
2026-09-10 08:40:49 +0300

    win server startup script update

--- oracle-jde-framework/vm.tf
+++ oracle-jde-framework/vm.tf
@@ -364,7 +364,7 @@ resource "google_compute_instance" "jde_demo_dep" {
 
   metadata = {
     enable-oslogin = "TRUE"
-    startup-script = file("${path.module}/scripts/jde-win-startup.bat")
+    windows-startup-script-ps1 = file("${path.module}/scripts/jde-win-startup.bat")
   }
 
   tags = local.vm_network_tags.vision
